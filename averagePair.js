47043e169550433e1f68ab13ffcf45b2979b5821
othaderek <otha.hernandez@gmail.com>
2019-09-02 16:24:40 -0400

    added more set cases.

--- averagePair.js
+++ averagePair.js
@@ -9,6 +9,7 @@ const averagePair = (array, targetAverage) => {
     let avg = (array[lPointer] + array[rPointer]) / 2;
     if (avg === targetAverage){
       console.log(true);
+      console.log(array[lPointer], array[rPointer]);
       return true;
     } else if (avg < targetAverage){
       lPointer++
@@ -21,4 +22,6 @@ const averagePair = (array, targetAverage) => {
 }
 
 
-averagePair([1,2,3,4,6], 3);
+averagePair([1,2,3,4,6], 3); // => true
+averagePair([1,6], 3); ; // => false
+averagePair([-3,-2,0,4,6,13,22], 3); ; // => false
