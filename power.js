2cf2524899469d95608f029cafac3dfc253429da
othaderek <otha.hernandez@gmail.com>
2019-09-05 21:41:00 -0400

    Initial commit.

---
+++ power.js
@@ -0,0 +1,12 @@
+/*
+  Power
+
+  Write a recursive function that takes a base
+  and exponent and returns the result of a given base
+  to a given power.
+
+*/
+
+const power = (base, exponent) => {
+
+}
