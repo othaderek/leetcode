4b66269253ec13f4a267d16f24bded32457617d3
othaderek <otha.hernandez@gmail.com>
2019-09-05 22:01:18 -0400

    Added description of linear search function.

--- linearSearch.js
+++ linearSearch.js
@@ -1,4 +1,13 @@
+/*
 
+  Linear Search
+
+  Given an array and a target, perform linear search
+  to check if the given target is in the array
+  If target number in array return the index of the number
+  Else, return -1.
+
+*/
 
 const linearSearch = (array, target) => {
   for (let i = 0; i < array.length; i++){
