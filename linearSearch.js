61112364520f0379c26f8c4b27592f2671587418
othaderek <otha.hernandez@gmail.com>
2019-09-05 22:03:43 -0400

    More concise if statement.

--- linearSearch.js
+++ linearSearch.js
@@ -11,9 +11,7 @@
 
 const linearSearch = (array, target) => {
   for (let i = 0; i < array.length; i++){
-    if (array[i] === target){
-      return i;
-    }
+    if (array[i] === target) return i;
   }
   return -1;
 }
