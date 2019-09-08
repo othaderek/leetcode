6e1b318e9a2714073b896c5d24acd5725b0de29a
othaderek <otha.hernandez@gmail.com>
2019-09-08 03:20:19 -0400

    Optimized to check if swap was made.

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,14 +1,17 @@
 const bubbleSort = (array) => {
+  let noSwaps;
   const swap = (array, index1, index2) => {
     [array[index1], array[index2]] = [array[index2], array[index1]];
   };
-
   for (let i = array.length; i > 0; i--){
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++){
       if (array[j] > array[j + 1]){
         swap(array, j, j + 1);
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   console.log(array);
   return array;
