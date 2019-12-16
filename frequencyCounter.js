741fa2b9f3b27255b6dbf1a6c175a9a5274fcc92
othaderek <otha.hernandez@gmail.com>
2019-12-16 13:16:23 -0500

    Added comments above the block of code, and changed some of the variable names.

---
+++ frequencyCounter.js
@@ -0,0 +1,20 @@
+// This is an example of the frequency counter pattern
+// The purpose of this pattern is to count the frequency of elements in an array
+// It does this by using a 'for of' loop that iterates through the given array
+// In a for of loop, it takes the 'value' of 'array which is in the index position
+// of the current iteration and checks to see if it is in the object map
+// on line 11. This line of code creates a key of the value and assigns one to the value
+// of this newly created key, OR it returns the value of the already existing key and increments
+// that value by one.
+
+const frequencyCounter = (array) => {
+    let objectMap = {};
+
+    for (const value of array) {
+        objectMap[value] = (objectMap[value] || 0) + 1
+    }
+    console.log(objectMap);
+    
+}
+
+frequencyCounter([1,2,3,4,3,3,1])
